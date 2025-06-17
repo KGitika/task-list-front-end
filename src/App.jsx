e12c250065bd08f859325d079db49c8afd985180
Gitika Kalal <gitikakalal21@gmail.com>
2025-06-17 00:16:53 -0700

    Lifted task state to App component and implemented toggle & delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -15,13 +16,38 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  // Toggle isComplete field of a task
+  const toggleTaskComplete = (id) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isComplete: !task.isComplete };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  // Delete a task
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {<TaskList
+            tasks={tasks}
+            onToggleComplete={toggleTaskComplete}
+            onDeleteTask={deleteTask}
+          />}
+        </div>
       </main>
     </div>
   );
