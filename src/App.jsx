c2788b5a0f8ba9243cad6a169e7f91f597805f0c
sashavershkova <a.m.vershkova@gmail.com>
2025-06-17 18:04:57 -0700

    add endpoints: get, delete, complete/incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,85 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import TaskList from './components/TaskList.jsx';
 import './App.css';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// save base URL of our local host
+const kBaseURL='http://127.0.0.1:5000';
 
+// call API to get all the tasks
+const getTasksViaApi = () => {
+  return axios.get(`${kBaseURL}/tasks`)
+    .then(response => {
+      return response.data;
+    })
+    .catch (error => {
+      console.log(error);
+      throw new Error('Unable to fetch tasks');
+    });
+};
+
+// call API to delete a task by id
+const deleteTaskViaApi = (taskId) => {
+  return axios.delete(`${kBaseURL}/tasks/${taskId}`)
+    .then(response => {
+      console.log(response);
+    })
+    .catch (error => {
+      console.log(error);
+      throw new Error('Unable to delete task');
+    });
+};
+
+// call API to toggle completeness
+const toggleCompleteViaApi = (taskId, markState) => {
+  return axios.patch(`${kBaseURL}/tasks/${taskId}/${markState}`)
+    .then(response => {
+      console.log(response);
+    })
+    .catch (error => {
+      console.log(error);
+      throw new Error(`Unable to mark task ${markState}`);
+    });
+};
+
+// App component
 const App = () => {
-  const [tasks, setTasks] = useState(TASKS);
+  const [tasks, setTasks] = useState([]);
+
+  // get all the tasks
+  useEffect(() => {
+    console.log('we are inside use effect');
+    getTasksViaApi()
+      .then(data => {setTasks(data);})
+      .catch(error => {console.log('Erorr fetching tasks:', error);});
+  }, []);
+
+  // delete task by id
   const deleteTask = (taskId) => {
-    const updatedTasks = tasks.filter(task => task.id !== taskId);
-    setTasks(updatedTasks);
+    deleteTaskViaApi(taskId)
+      .then(() => {return getTasksViaApi()})
+      .then(data => {setTasks(data)})
+      .catch(error => {console.log('can not delete task', error)});
   };
 
+  // toggle complete
   const toggleCompleteness = (taskId) => {
-    const updatedTasks = tasks.map(task => {
-      if (task.id === taskId) {
-        return {...task, isComplete: !task.isComplete};
-      } else {
-        return task;
-      }
-    });
-    setTasks(updatedTasks);
+    const taskToUpdate = tasks.find(task => task.id === taskId);
+    if (!taskToUpdate) {
+      return;
+    }
+    // choose endpoint depending on current state
+    const endPoint = !taskToUpdate.is_complete ? 'mark_complete' : 'mark_incomplete';
+    toggleCompleteViaApi(taskId, endPoint)
+      .then(() => {
+        return getTasksViaApi();
+      })
+      .then(data => {
+        setTasks(data);
+      })
+      .catch(error => {
+        console.log('Failed to toggle completeness:', error);
+      });
   };
 
   return (
@@ -39,7 +88,7 @@ const App = () => {
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={tasks} completeness={toggleCompleteness} deleteTask={deleteTask} />}</div>
+        <div><TaskList tasks={tasks} completeness={toggleCompleteness} deleteTask={deleteTask} /></div>
       </main>
     </div>
   );
